e1f6003677fec59f87c6da47664e65a2c3785f29
Paratra <ming.song.cn@outlook.com>
2021-10-14 14:31:33 -0400

    update

--- src/main.py
+++ src/main.py
@@ -92,20 +92,20 @@ ser = serial.Serial(
 #ser.write(b"STOP;")
 #time.sleep(1)
 
-# ser.write(b"RESET;")
-# time.sleep(0.5)
-#
-# ser.write(b"FORMAT=D;")
-# time.sleep(0.5)
-#
-# ser.write(b"CHANNEL=1;")
-# time.sleep(0.5)
-#
-# ser.write(b"SAMPLERATE=80;")
-# time.sleep(0.5)
-#
-# ser.write(b"START;")
-# time.sleep(0.5)
+ser.write(b"RESET;")
+time.sleep(0.5)
+
+ser.write(b"FORMAT=D;")
+time.sleep(0.5)
+
+ser.write(b"CHANNEL=1;")
+time.sleep(0.5)
+
+ser.write(b"SAMPLERATE=80;")
+time.sleep(0.5)
+
+ser.write(b"START;")
+time.sleep(0.5)
 
 
 
