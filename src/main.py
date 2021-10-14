60bfb7115904f441a4f330c17d0b844fa43f64b3
Paratra <ming.song.cn@outlook.com>
2021-10-14 17:52:08 -0400

    update

--- src/main.py
+++ src/main.py
@@ -143,6 +143,7 @@ while(ser.isOpen()):
 
    ### write use write_influx
     current_time_stamp = datetime.datetime.now(pytz.UTC)
+    st()
     write_influx(dest, 'greenboard', 'Z', 'value', data_list, current_time_stamp, fs=80)
     # for point in data_list:
 
